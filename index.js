297160e22619174ee8dbb63e8695d27a315e3617
Dmitriy Velichko <dmitriy.velichko.dev@gmail.com>
2019-08-20 16:20:43 +0300

    configure eslint

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-let path = require('path');
-const fileName = path.basename(__filename);
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const { routingHandler } = './lib/router';
+
+const config = require('./config');
+
+const httpServer = http.createServer(routingHandler);
+
+httpServer.listen(config.httpPort, () => {
+	console.log(`The HTTP server is running on port ${config.httpPort}`);
+});
+
+const httpsServerOptions = {
+	key: fs.readFileSync('./https/key.pem'),
+	cert: fs.readFileSync('./https/cert.pem'),
+};
+
+const httpsServer = https.createServer(httpsServerOptions, routingHandler);
+
+httpsServer.listen(config.httpsPort, () => {
+	console.log(`The HTTPS server is running on port ${config.httpsPort}`);
+});

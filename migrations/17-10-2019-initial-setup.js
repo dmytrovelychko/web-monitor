3ebd6383407cae1977d48be31f773819365659d3
dmitriyvelichkodev <dmitriy.velichko.dev@gmail.com>
2019-10-20 08:11:15 +0300

    add updateAt, createdAt to tables

--- migrations/17-10-2019-initial-setup.js
+++ migrations/17-10-2019-initial-setup.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
 	return knex.schema
-		.createTable('user', table => {
+		.createTable('users', table => {
 			table.specificType('id', 'char(36)').primary();
 			table.string('firstName', 255).notNullable();
 			table.string('lastName', 255);
@@ -10,28 +10,58 @@ exports.up = function(knex) {
 				.notNullable();
 			table.string('password', 255).notNullable();
 			table.string('salt', 255).notNullable();
+			table
+				.timestamp('createdAt')
+				.notNullable()
+				.defaultTo(knex.fn.now());
+			table
+				.timestamp('updatedAt')
+				.notNullable()
+				.defaultTo(
+					knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+				);
 		})
-		.createTable('token', table => {
+		.createTable('tokens', table => {
 			table.specificType('id', 'char(36)').primary();
 			table.specificType('userId', 'char(36)').notNullable();
-			table.foreign('userId').references('user.id');
+			table.foreign('userId').references('users.id');
 			table.dateTime('expire').notNullable();
+			table
+				.timestamp('createdAt')
+				.notNullable()
+				.defaultTo(knex.fn.now());
+			table
+				.timestamp('updatedAt')
+				.notNullable()
+				.defaultTo(
+					knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+				);
 		})
-		.createTable('check', table => {
+		.createTable('checks', table => {
 			table.specificType('id', 'char(36)').primary();
 			table.specificType('userId', 'char(36)').notNullable();
-			table.foreign('userId').references('user.id');
+			table.foreign('userId').references('users.id');
 			table.string('protocol', 10);
 			table.string('url', 255);
 			table.string('method', 255);
 			table.string('successCodes', 255);
 			table.integer('timeoutSec');
+			table
+				.timestamp('createdAt')
+				.notNullable()
+				.defaultTo(knex.fn.now());
+			table
+				.timestamp('updatedAt')
+				.notNullable()
+				.defaultTo(
+					knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+				);
 		});
 };
 
 exports.down = function(knex) {
 	return knex.schema
-		.dropTable('check')
-		.dropTable('token')
-		.dropTable('user');
+		.dropTable('checks')
+		.dropTable('tokens')
+		.dropTable('users');
 };
